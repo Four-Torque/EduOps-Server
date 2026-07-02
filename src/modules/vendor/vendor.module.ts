8828462a5103796ffe3747783a7ef501226848ac
Jung Woon Jik <dnswlrsla@gmail.com>
2026-07-02 14:14:56 +0900

    fix: 구매처 repository provider 추가

--- src/modules/vendor/vendor.module.ts
+++ src/modules/vendor/vendor.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { VendorService } from './service/vendor.service';
 import { VendorController } from './controller/vendor.controller';
+import { VendorRepository } from './repository/vendor.repository';
 
 @Module({
   controllers: [VendorController],
-  providers: [VendorService],
+  providers: [VendorService, VendorRepository],
 })
 export class VendorModule {}
