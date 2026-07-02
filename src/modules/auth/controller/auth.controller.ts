06b7078de901ff5af7fa51377d274d2dbbc13a06
Minha Lee <fgz1234@gmail.com>
2026-07-02 10:19:06 +0900

    [fix] 로그아웃
    인증우회 제외

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -32,12 +32,12 @@ import {
 import { User } from '@prisma/client';
 import { EmailResponse } from '../response/email.response';
 
-@Public()
 @ApiTags('인증')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @ApiOperation({
     summary: '회원가입',
     description: '회원가입을 위한 이메일이 발송됩니다.',
@@ -50,6 +50,7 @@ export class AuthController {
     await this.authService.register(request);
   }
 
+  @Public()
   @ApiOperation({
     summary: '이메일 인증',
     description: '이메일 인증 후 회원가입이 완료됩니다.',
@@ -69,6 +70,7 @@ export class AuthController {
     await this.authService.verify(token, type);
   }
 
+  @Public()
   @ApiOperation({
     summary: '로그인',
     description: '이메일과 비밀번호로 로그인을 진행합니다.',
@@ -99,6 +101,7 @@ export class AuthController {
     clearCookies(res);
   }
 
+  @Public()
   @ApiOperation({
     summary: '토큰 재발급',
     description: '리프레시 토큰을 이용하여 액세스 토큰을 재발급합니다.',
@@ -116,6 +119,7 @@ export class AuthController {
     return setCookies(response, accessToken, refreshToken);
   }
 
+  @Public()
   @ApiOperation({
     summary: '비밀번호 재설정 이메일 발송',
     description: '비밀번호 재설정을 위한 이메일을 발송합니다.',
@@ -128,6 +132,7 @@ export class AuthController {
     await this.authService.sendResetPasswordMail(email);
   }
 
+  @Public()
   @ApiOperation({
     summary: '비밀번호 재설정 토큰 검증',
     description: '비밀번호 재설정을 위한 토큰을 검증합니다.',
@@ -141,6 +146,7 @@ export class AuthController {
     return response;
   }
 
+  @Public()
   @ApiOperation({
     summary: '비밀번호 재설정',
     description: '비밀번호를 재설정합니다.',
