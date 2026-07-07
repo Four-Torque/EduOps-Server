63c868a04e3749104d6b3373f540c1943ff4e957
Jung Woon Jik <dnswlrsla@gmail.com>
2026-07-07 10:33:06 +0900

    feat: 수업 파일 CRUD

--- src/global/enums/error-code.enum.ts
+++ src/global/enums/error-code.enum.ts
@@ -55,6 +55,11 @@ export enum ErrorCode {
 
   // 시험 관련 에러
   EXAM_NOT_FOUND = 'EXAM_001',
+
+  // 수업 파일 관련 에러
+  CLASS_FILE_NOT_FOUND = 'CLASS_FILE_001',
+  FILE_NOT_PROVIDED = 'CLASS_FILE_002',
+  CLASS_FILE_NOT_FOUND_ON_DISK = 'CLASS_FILE_003',
 }
 
 export const ErrorCodeMap: Record<
@@ -201,5 +206,19 @@ export const ErrorCodeMap: Record<
   [ErrorCode.EXAM_NOT_FOUND]: {
     status: HttpStatus.NOT_FOUND,
     message: '해당 시험을 찾을 수 없습니다.',
+  },
+
+  // 수업 파일 관련 에러
+  [ErrorCode.CLASS_FILE_NOT_FOUND]: {
+    status: HttpStatus.NOT_FOUND,
+    message: '해당 수업 파일을 찾을 수 없습니다.',
+  },
+  [ErrorCode.FILE_NOT_PROVIDED]: {
+    status: HttpStatus.BAD_REQUEST,
+    message: '파일이 제공되지 않았습니다.',
+  },
+  [ErrorCode.CLASS_FILE_NOT_FOUND_ON_DISK]: {
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    message: '해당 파일을 디스크에서 찾을 수 없습니다.',
   },
 };
